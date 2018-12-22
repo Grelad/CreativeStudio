f20dd00a0c80caabc40e38e6c5fa8453748ef3ba
Grelad <grechenenko@gmail.com>
2018-12-22 14:19:51 +0200

    try to deploy heroku 2

--- landing/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from .forms import SubscriberForm
-
-
-def landing(request):
-    form = SubscriberForm(request.POST or None)
-    if request.method == "POST" and form.is_valid():
-        new_form = form.save()
-    return render(request, 'landing/index.html', locals())
