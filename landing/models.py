4fc784222f08a583a9a1e8766a7d69f2409a1354
Grelad <grechenenko@gmail.com>
2019-01-02 20:57:51 +0200

    change models

--- landing/models.py
+++ landing/models.py
@@ -5,6 +5,3 @@ class Subscriber(models.Model):
     email = models.EmailField()
     name = models.CharField(max_length=64)
     number = models.CharField(max_length=13)
-
-    def __str__(self):
-        return "Пользователь %s %s" % (self.name, self.email)
